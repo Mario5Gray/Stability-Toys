d1b8ee6cb1d63e1e58fcd9ab395534f6dab35a2d
Mario Gray <mario.gray@gmail.com>
2026-03-29 21:39:48 -0700

    dev update, modes

--- lcm-sr-ui/vite.config.js
+++ lcm-sr-ui/vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const apiTarget = process.env.VITE_API_TARGET ?? "http://localhost:4200";
+
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,22 +16,22 @@ export default defineConfig({
   server: {
     allowedHosts: process.env.VITE_ALLOWED_HOSTS 
       ? process.env.VITE_ALLOWED_HOSTS.split(',')
-      : ["mindgate", "enigma", "node2", "enigma:5173"],
+      : ["mindgate", "enigma", "node2", "enigma:5173","mindgate:4200"],
     host: true,
     watch: {
       usePolling: true,
       interval: 300,
     },    
     proxy: {
-      "/generate": "http://localhost:4200",
-      "/superres": "http://localhost:4200",
+      "/generate": apiTarget,
+      "/superres": apiTarget,
       "/v1": {
-        target: "http://localhost:4200",
+        target: apiTarget,
         ws: true,
       },
-      "/storage": "http://localhost:4200",
-      "/dreams": "http://localhost:4200",
-      "/api": "http://localhost:4200",
+      "/storage": apiTarget,
+      "/dreams": apiTarget,
+      "/api": apiTarget,
     },
   },
 });
